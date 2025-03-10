138cf71af6b6d68c34fa657772c8bf0ec6e57a4a
Yoel Fatal <yp3yp3@gmail.com>
2025-03-10 12:01:50 +0200

    fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-rds-234554"
+    key            = "default/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
